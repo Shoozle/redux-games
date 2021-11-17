cfe5aa49f28961d6cc759f55b0c6d3e79b79125e
Shoozle <sean@seanmurtagh.uk>
2021-11-17 14:07:36 +0000

    Remove dotenv reques from app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { loadGames } from "./actions/gamesAction";
-require('dotenv').config();
 
 function App() {
   
