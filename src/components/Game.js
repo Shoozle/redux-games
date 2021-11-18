e08c07fd5032c53d17535a1f21addd7f8d28bbd7
Shoozle <sean@seanmurtagh.uk>
2021-11-18 03:14:47 +0000

    styled games

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,12 +5,26 @@ import { motion } from "framer-motion";
 const Game = ({name, released, image}) => {
 
     return (
-        <div>
+        <StyledGame>
             <h3>{name}</h3>
             <p>{released}</p>
             <img src={image} alt={name} />
-        </div>
+        </StyledGame>
     )
 }
+
+const StyledGame = styled(motion.div)`
+    min-height: 30vh;
+    box-shadow: 0px 5px 20px rgba(0, 0, 0, .1);
+    overflow: hidden;
+    text-align: center;
+    border-radius: 1rem;
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        object-position: 50%;
+    }
+`
 
 export default Game;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -30,11 +30,18 @@ const Home = () => {
 }
 
 const GameList = styled(motion.div)`
-
+    padding: 0rem 5rem;
+    h2 {
+        padding: 5rem 0rem;
+    }
 `
 
 const Games = styled(motion.div)`
-
+    min-height: 80vh;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+    column-gap: 3rem;
+    row-gap: 5rem;
 `
 
 
