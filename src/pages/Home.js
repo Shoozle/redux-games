b0b351b885dee03eb8533a72174a5d256421a909
Shoozle <sean@seanmurtagh.uk>
2021-11-18 02:59:53 +0000

    Render games upcoming

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,10 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
 import { loadGames } from "../actions/gamesAction";
+import Game from "../components/Game";
 
 const Home = () => {
 
@@ -8,15 +12,30 @@ const Home = () => {
 
     useEffect(() => {
       dispatch(loadGames())
-    }, [])
+    }, [dispatch])
 
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+    //get data back
+    const {popular, newGames, upcoming} = useSelector(state => state.games)
     
 
+    return (
+        <GameList>
+            <Games>
+                {upcoming.map(game => (
+                    <Game name={game.name} released={game.released} key={game.id} id={game.id} image={game.background_image}/>
+                ))}
+            </Games>
+        </GameList>
+    )
 }
+
+const GameList = styled(motion.div)`
+
+`
+
+const Games = styled(motion.div)`
+
+`
+
 
 export default Home;
