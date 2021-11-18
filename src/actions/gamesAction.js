6f183687f2ca394c62d06f1599deec06c6318058
Shoozle <sean@seanmurtagh.uk>
2021-11-18 02:05:23 +0000

    new api requests

--- src/actions/gamesAction.js
+++ src/actions/gamesAction.js
@@ -1,15 +1,19 @@
 import axios from "axios";
-import { popularGamesURL } from '../api';
+import { popularGamesURL, newGamesURL, upcomingGamesURL } from '../api';
 
 export const loadGames = () => async (dispatch) => {
     //returns string to make api request
     const popularData = await axios.get(popularGamesURL());
+    const newGamesData = await axios.get(newGamesURL());
+    const upcomingData = await axios.get(upcomingGamesURL());
     
     //dispatches an action of type fetch_games and the payload data into the popular array state
     dispatch({
         type: "FETCH_GAMES",
         payload: {
-            popular: popularData.data.results
+            popular: popularData.data.results,
+            upcoming: upcomingData.data.results,
+            newGames: newGamesData.data.results
         }
     })
 }
--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -1,14 +1,18 @@
 const initialState = {
     popular : [],
-    newGames : [],
     upcoming : [],
+    newGames : [],
     searched: []
 }
 
 const gamesReducer = (state = initialState, action) => {
     switch (action.type) {
         case "FETCH_GAMES": 
-            return {...state, popular: action.payload.popular};
+            return {...state, 
+                popular: action.payload.popular, 
+                upcoming: action.payload.upcoming, 
+                newGames: action.payload.newGames
+            };
         default:
             return {...state};
     }
