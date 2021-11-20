f650f279596d553f02299eea319c54f465f109cf
Shoozle <sean@seanmurtagh.uk>
2021-11-20 11:29:49 +0000

    Fixed old data display bug

--- src/actions/detailAction.js
+++ src/actions/detailAction.js
@@ -3,12 +3,13 @@ import { gameDetailsURL, gameImagesURL } from "../api";
 
 export const loadDetail = (id) => async (dispatch) => {
     //Fetch url from api with id we pass in here
-    const detailData = await axios.get(gameDetailsURL(id))
-    const screenshotData = await axios.get(gameImagesURL(id))
 
     dispatch({
         type: "LOADING_DETAIL"
     })
+
+    const detailData = await axios.get(gameDetailsURL(id))
+    const screenshotData = await axios.get(gameImagesURL(id))
 
     dispatch({
         type: "GET_DETAIL",
